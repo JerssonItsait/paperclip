b80866b6a47c5e65e57838f2f825ae5f51632e8b
JerssonRestrepo <alejoalzate@misena.edu.co>
2013-04-22 14:22:34 -0500

    tabla basica con bootstrap,simple-form y gema para subir imagenes

---
+++ foto/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foto::Application.config.secret_token = 'd159ed4fabd1d3ce4f69a2515c91bef6597df2e74cf8713af70296b745f6a0885aeee275fe5f57418fa9001318d9c90f546e87ba2e1c9e99753786ec7a7f18fb'
